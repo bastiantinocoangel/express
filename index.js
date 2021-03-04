3aa13435e9030790bae15125b5a05e1ad705d56e
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2021-03-04 16:46:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('UTD');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('UTD')
+})
+
+app.listen(port, () => {
+  console.log(`Ejemplo en http://localhost:${port}`)
+})
